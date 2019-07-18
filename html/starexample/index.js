ced15c8cdf4ae331aefe7ab1b5f85b8e14f36549
kuuku123 <dohyun0047@gmail.com>
2019-07-18 19:03:01 +0900

    별모양 옆에 드디어 숫자가!

--- html/starexample/index.js
+++ html/starexample/index.js
@@ -2,6 +2,6 @@ $('.starRev span').click(function(){
   $(this).parent().children('span').removeClass('on');
   $(this).addClass('on').prevAll('span').addClass('on');
   
-  $(this).parent().parent().prop('value', $(this).parent().parent().text( $(this).addClass('on').prevAll('span').length+1))
+  $(this).parent().children('b').html( $(this).addClass('on').prevAll('span').length+1);
   return false;
 });
