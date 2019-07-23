cf9ad5f2b29f13bb0628807f30db18ebdd1bd89f
Hyeon Jeon <jeonhyun97@gmail.com>
2019-07-24 05:06:04 +0900

    test

--- html/operator/js/BCRiders.js
+++ html/operator/js/BCRiders.js
@@ -1,4 +1,8 @@
 function validateAddress(){
     var validAddress = document.getElementById("validation").value;
-    BCRiders.addValidAddress(validAddress,function(){} );
+    BCRiders.addValidAddress(validAddress,function(err, result){
+        if(!errr) {
+            alert("Validation success!!");
+        }
+    } );
 };
