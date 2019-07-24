1ecd393a7460fe4a7dab5f876363207d00d11d53
Hyeon Jeon <jeonhyun97@gmail.com>
2019-07-24 18:15:18 +0900

    test

---
+++ html/signUp/js/BCRidersHelper.js
@@ -0,0 +1,42 @@
+var nickname = document.getElementById("nickname").value;
+var name = document.getElementById("name").value;
+var department = document.getElementById("department").value;
+
+
+function startApp() {
+    contract = web3.eth.contract(contractABI);
+    BCRiders = contract.at(contractAddress);
+}
+
+function signUpAccount() {
+    var a;
+    switch(department){
+        case "Mathematics": a = 1;
+        break;
+        case  "Physics": a = 2;
+        break;
+        case "Chemistry": a= 3;
+        break;
+        case "Life Science": a = 4;
+        break;
+        case "Matrials Science & Engineering": a= 5;
+        break;
+        case "Mechanical Engineering": a = 6;
+        break;
+        case "Industrial and Management Engineering": a = 7;
+        break;
+        case "Electronic and Electric Engineering": a = 8;
+        break;
+        case "Computer Science and Engineering": a = 9;
+        break;
+        case "Chemical Engineering": a = 10;
+        break;
+        case "Creative IT Engineering": a = 11;
+        break;
+    }
+    BCRiders.requestStuRegister(nickname, name, a, function(err, result) {
+        if(result == true) {
+            alert("Sign Up success! Hello, "+name+"!!");
+        }
+    });
+}
